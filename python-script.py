ee817d5a95b1b1873f935a5b099660e84060791a
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2017-02-16 13:36:58 +0530

    Changed the py file and validation

--- python-script.py
+++ python-script.py
@@ -1,8 +1,23 @@
 import pdfkit
 import sys
 
-print(sys.argv[1])
 try:
-    pdfkit.from_file(sys.argv[1], 'out.pdf')
-except Error as e:
-    print(e)
+    input = sys.argv[1]
+except:
+    print("Missing input file")
+    sys.exit()
+
+try:
+    output = sys.argv[2]
+except:
+    print("Missing output file")
+    sys.exit()
+
+
+def main():
+    try:
+        pdfkit.from_file(input, output)
+    except IOError as error:
+        print (error)
+
+if __name__ == "__main__": main()
