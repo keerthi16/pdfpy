3a9ebdeeabad17da5225c4bd208da0319772153e
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2017-02-18 23:14:57 +0530

    Version 2.0.2 .sh

--- startup.sh
+++ startup.sh
@@ -5,6 +5,14 @@ case $OS in
     OS='Linux'
     curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
     python get-pip.py
+    apt update
+    apt install wkhtmltopdf
+    sudo apt-get install -y openssl build-essential xorg libssl-dev
+    wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+    tar -xJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+    cd wkhtmltox
+    sudo chown root:root bin/wkhtmltopdf
+    sudo cp -r * /usr/
     pip install pdfkit
     pip install simplejson
     ;;
