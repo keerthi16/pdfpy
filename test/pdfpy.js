d4c39aa7b099fd39b63e1a679790e002257192f6
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2017-02-19 17:28:54 +0530

    Test

--- test/pdfpy.js
+++ test/pdfpy.js
@@ -11,6 +11,7 @@ describe("Agent Test", function () {
 
     it("From url it should create a pdf file", function (done) {
 
+        this.timeout(5000);
         var data = {
             input: "google.com",
             output: "./out.pdf"
@@ -20,12 +21,14 @@ describe("Agent Test", function () {
             should.not.exist(err);
             should.exist(fs.existsSync(data.output));
             should.exist(res);
+            fs.unlinkSync(data.output)
+            done();
         });
-        done();
     });
 
     it("From html file it should create a pdf file", function (done) {
 
+        this.timeout(5000);
         var data = {
             input: "./example/example.html",
             output: "./example.pdf"
@@ -36,7 +39,39 @@ describe("Agent Test", function () {
             should.exist(fs.existsSync(data.output));
             console.log(fs.existsSync(data.output));
             should.exist(res);
+            fs.unlinkSync(data.output);
+            done();
         });
-        done();
+    });
+
+    it("Incorrect path", function (done) {
+
+        this.timeout(5000);
+        var data = {
+            input: "../example/example.html",
+            output: "./example.pdf"
+        };
+
+        pdf.file(data, function (err, res) {
+            should.exist(err);
+            should.exist(!fs.existsSync(data.output));
+            should.not.exist(res);
+            done();
+        });
+    });
+
+    it("In put path not provided", function (done) {
+
+        this.timeout(5000);
+        var data = {
+            output: "./example.pdf"
+        };
+
+        pdf.file(data, function (err, res) {
+            should.exist(err);
+            should.exist(!fs.existsSync(data.output));
+            should.not.exist(res);
+            done();
+        });
     });
 });
