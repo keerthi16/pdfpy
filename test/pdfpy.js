39b56e334df3be52116e99528aecf2a42a9a3a64
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2017-02-24 14:29:38 +0530

    Test

--- test/pdfpy.js
+++ test/pdfpy.js
@@ -21,9 +21,9 @@ describe("Agent Test", function () {
             should.not.exist(err);
             should.exist(fs.existsSync(data.output));
             should.exist(res);
-            fs.unlinkSync(data.output)
+            fs.unlinkSync(data.output);
+            done();
         });
-        done();
     });
 
     it("From html file it should create a pdf file", function (done) {
@@ -40,8 +40,8 @@ describe("Agent Test", function () {
             console.log(fs.existsSync(data.output));
             should.exist(res);
             fs.unlinkSync(data.output);
+            done();
         });
-        done();
     });
 
     it("Incorrect path", function (done) {
